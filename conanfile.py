d084b9c5ed4e0e5e92a81070feb2763f25742555
Abdulla Abakarov <abakarov97@gmail.com>
2025-09-21 14:53:13 +0400

    Add `conanfile.py`

---
+++ conanfile.py
@@ -0,0 +1,49 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+
+
+class guiTestProjectRecipe(ConanFile):
+    name = "gui_test_project"
+
+    options = {
+        "enableLog": [True, False],
+        "useCcache": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "enableLog": False,
+        "useCcache": True,
+        "fPIC": True,
+    }
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    def requirements(self):
+        # self.requires("sdl/3.2.20")
+        return
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+
+        tc.variables["ENABLE_LOG"] = self.options["enableLog"]
+
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        return
+
+    def package_info(self):
+        return
