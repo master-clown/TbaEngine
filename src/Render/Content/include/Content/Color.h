dae05ba53168188681af33065aff1cdb2aee367a
Abdulla Abakarov <abakarov97@gmail.com>
2025-11-17 18:22:23 +0400

    Add some predefined color constants to `content::Color`

--- src/Render/Content/include/Content/Color.h
+++ src/Render/Content/include/Content/Color.h
@@ -9,5 +9,9 @@ namespace content {
         uint8 g = 0;
         uint8 b = 0;
         uint8 a = 0;
+
+        static const Color transparent;
+        static const Color black;
+        static const Color white;
     };
 }
