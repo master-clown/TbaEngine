1f241b7db264f55e1484d55a92f9ee1ca6f0265b
Abdulla Abakarov <abakarov97@gmail.com>
2025-11-20 19:02:01 +0400

    Add `Common/Stl/Array.h`

---
+++ src/Common/include/Common/Stl/Array.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+
+//======================================================================================================================
+template <class T, size_t arraySize>
+using Array = std::array<T, arraySize>;
