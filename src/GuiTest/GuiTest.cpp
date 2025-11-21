48420c1c914762ad6a7a709dda40fbb511075ade
Abdulla Abakarov <abakarov97@gmail.com>
2025-11-21 18:17:25 +0400

    Draw a line!

--- src/GuiTest/GuiTest.cpp
+++ src/GuiTest/GuiTest.cpp
@@ -3,7 +3,18 @@
 #include <AppEvent/EventInfo.hpp>
 #include <Common/Memory.h>
 #include <Framework/SdlFramework.h>
+#include <Geometry2d/Line.h>
+#include <Geometry2d/Point2d.h>
+#include <Geometry2d/Triangle.h>
+#include <Render2d/Renderer.h>
+#include <Renderer/Renderer.h>
 #include <Winsys/WindowMgr.h>
+
+//======================================================================================================================
+GuiTest::GuiTest() = default;
+
+//----------------------------------------------------------------------------------------------------------------------
+GuiTest::~GuiTest() = default;
 
 //======================================================================================================================
 uptr<framework::Framework> GuiTest::_init()
@@ -15,11 +26,23 @@ uptr<framework::Framework> GuiTest::_init()
         .wndHeight = 480,
     });
 
+    _renderer = fr->createRenderer(render::RendererType::Sdl, *_appWindow);
+
     return fr;
 }
 
 //======================================================================================================================
 auto GuiTest::_iterate(const app_event::AppEvent& appEventVariant) -> ShouldQuit
+{
+    const auto shouldQuit = _processEvent(appEventVariant);
+
+    _render();
+
+    return shouldQuit;
+}
+
+//======================================================================================================================
+auto GuiTest::_processEvent(const app_event::AppEvent& appEventVariant) -> ShouldQuit
 {
     if (const auto* windowEvent = std::get_if<app_event::WindowEvent>(&appEventVariant)) {
         using Kind = app_event::WindowEventKind;
@@ -29,4 +52,21 @@ auto GuiTest::_iterate(const app_event::AppEvent& appEventVariant) -> ShouldQuit
     }
 
     return ShouldQuit{false};
+}
+
+//======================================================================================================================
+void GuiTest::_render()
+{
+    _renderer->clear(content::Color::black);
+
+    auto& renderer2d = _renderer->get2dRenderer();
+
+    const auto line = render_2d::RenderableGeometry<geometry_2d::Line>{
+        .primitive = geometry_2d::Line{.startPt = {.x = 100, .y = 100},
+                                       .finalPt = {.x = 400, .y = 400}},
+    };
+
+    renderer2d.render(line);
+
+    _renderer->finalizeRender();
 }
--- src/GuiTest/GuiTest.h
+++ src/GuiTest/GuiTest.h
@@ -4,11 +4,25 @@
 #include <Winsys/Window.h>
 
 //======================================================================================================================
+namespace render {
+    class Renderer;
+}
+
+//======================================================================================================================
 class GuiTest final : public framework::Application {
+public:
+    GuiTest();
+    ~GuiTest();
+
 private:
     uptr<framework::Framework> _init() override;
     ShouldQuit _iterate(const app_event::AppEvent&) override;
 
 private:
+    ShouldQuit _processEvent(const app_event::AppEvent&);
+    void _render();
+
+private:
     uptr<winsys::Window> _appWindow;
+    uptr<render::Renderer> _renderer;
 };
