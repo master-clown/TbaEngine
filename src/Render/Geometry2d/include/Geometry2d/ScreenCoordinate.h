77c0b2313f2497774906fe031c1a531036a380f3
Abdulla Abakarov <abakarov97@gmail.com>
2025-11-24 17:13:25 +0400

    Explain the "Screen CS" in `ScreenCoordinate.h`

--- src/Render/Geometry2d/include/Geometry2d/ScreenCoordinate.h
+++ src/Render/Geometry2d/include/Geometry2d/ScreenCoordinate.h
@@ -4,5 +4,9 @@
 
 //======================================================================================================================
 namespace geometry_2d {
+    // Coordinate on the "Screen CS"
+    // - Origin: top-left screen corner
+    // - Ox: to the right
+    // - Oy: down
     using ScreenCoordinate = uint16;
 }
