dabd6925749f5a617ede6c925c9e30be65998f2c
Abdulla Abakarov <abakarov97@gmail.com>
2025-12-08 18:30:54 +0400

    Implement the simplest logging functions

--- src/Logger/src/Logging.cpp
+++ src/Logger/src/Logging.cpp
@@ -1,1 +1,34 @@
 #include <Logger/Logging.h>
+
+#include <iostream>
+#include <mutex>
+
+//======================================================================================================================
+struct LogStreamContext final {
+    std::ostream* outStream = &std::cout;
+    std::mutex outStreamMutex;
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+static LogStreamContext globalLogStreamContext;
+
+//======================================================================================================================
+void logger::detail::logImpl(std::function<void(std::ostream&)> func)
+{
+    std::lock_guard lock{globalLogStreamContext.outStreamMutex};
+
+    // TODO: add timestamp
+
+    auto& outStream = *globalLogStreamContext.outStream;
+
+    func(outStream);
+}
+
+//======================================================================================================================
+void logger::detail::logError(std::function<void(std::ostream&)> func)
+{
+    logImpl([func = std::move(func)](std::ostream& stream) {
+        stream << "[ERROR] ";
+        func(stream);
+    });
+}
