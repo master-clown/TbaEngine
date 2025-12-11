2af99ededdbe5e2307af557b2174fd775967fb4e
Abdulla Abakarov <abakarov97@gmail.com>
2025-12-11 17:54:55 +0400

    Add `str(LogMessageLevel)`

--- src/Logger/src/LogMessageLevel.cpp
+++ src/Logger/src/LogMessageLevel.cpp
@@ -1,6 +1,22 @@
 #include <Logger/LogMessageLevel.h>
 
 #include <type_traits>
+#include <utility>
+
+//======================================================================================================================
+std::string logger::str(const logger::LogMessageLevel msg)
+{
+    using Enum = logger::LogMessageLevel;
+
+    switch (msg) {
+    case Enum::Silent: return "Silent";
+    case Enum::Brief: return "Brief";
+    case Enum::Normal: return "Normal";
+    case Enum::Verbose: return "Verbose";
+    case Enum::Trace: return "Trace";
+    default: std::unreachable();
+    }
+}
 
 //======================================================================================================================
 bool logger::operator<=(const LogMessageLevel lhs, const LogMessageLevel rhs)
