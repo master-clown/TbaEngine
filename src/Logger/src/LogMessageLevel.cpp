6b03e4bf606a41d5f96ca2e04e0ec9a3e4d88dca
Abdulla Abakarov <abakarov97@gmail.com>
2025-12-12 13:16:32 +0400

    Improve `str(LogMessageLevel)`

--- src/Logger/src/LogMessageLevel.cpp
+++ src/Logger/src/LogMessageLevel.cpp
@@ -9,11 +9,11 @@ std::string logger::str(const logger::LogMessageLevel msg)
     using Enum = logger::LogMessageLevel;
 
     switch (msg) {
-    case Enum::Silent: return "Silent";
-    case Enum::Brief: return "Brief";
-    case Enum::Normal: return "Normal";
-    case Enum::Verbose: return "Verbose";
-    case Enum::Trace: return "Trace";
+    case Enum::Silent: return "LogMessageLevel::Silent";
+    case Enum::Brief: return "LogMessageLevel::Brief";
+    case Enum::Normal: return "LogMessageLevel::Normal";
+    case Enum::Verbose: return "LogMessageLevel::Verbose";
+    case Enum::Trace: return "LogMessageLevel::Trace";
     default: std::unreachable();
     }
 }
