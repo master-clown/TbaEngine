95e38195a995e0e987468edd417eb917453a4872
Abdulla Abakarov <abakarov97@gmail.com>
2025-12-25 20:52:49 +0400

    Add trace log in `SdlAppEventMgr::update()` (if event is skipped)

--- src/AppEvent/src/SdlAppEventMgr.cpp
+++ src/AppEvent/src/SdlAppEventMgr.cpp
@@ -1,8 +1,10 @@
 #include <AppEvent/SdlAppEventMgr.h>
 
 #include <AppEvent/EventInfo.hpp>
+#include <AppEvent/LogCategory.h>
 
 #include <Common/Stl/Optional.h>
+#include <Logger/CategorizedLogging.h>
 
 #include <SDL3/SDL_events.h>
 
@@ -40,8 +42,13 @@ void SdlAppEventMgr::update()
         }
     }();
 
-    if (!newEvent.has_value())
+    if (!newEvent.has_value()) {
+        LOG_CATEGORIZED(LogCategory::EventConversionFromNative,
+                        logger::LogMessageLevel::Trace,
+                        "Skipping SDL_Event{{.type={:#X}}}",
+                        sdlEvent.type);
         return;
+    }
 
     getEventQueue().push(std::move(*newEvent));
 }
