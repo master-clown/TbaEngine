85de616c4a70caa393afdeb9f086974525414e00
Abdulla Abakarov <abakarov97@gmail.com>
2026-01-09 13:21:09 +0400

    Refactor `AppEventMgr` to use `NativeEventListeners`

--- src/AppEvent/AppEvent/include/AppEvent/AppEventMgr.h
+++ src/AppEvent/AppEvent/include/AppEvent/AppEventMgr.h
@@ -2,6 +2,15 @@
 
 #include <AppEvent/AppEvent.h>
 #include <AppEvent/EventQueue.h>
+#include <AppEvent/NativeEventListeners.h>
+
+#include <Common/Memory.h>
+#include <Common/Stl/Vector.h>
+
+//======================================================================================================================
+namespace app_event {
+    class NativeEvent;
+}
 
 //======================================================================================================================
 namespace app_event {
@@ -9,13 +18,23 @@ namespace app_event {
     public:
         virtual ~AppEventMgr();
 
-        virtual void update() = 0;
+        //--------------------------------------------------------------------------------------------------------------
+        void pollEvents();
 
         //--------------------------------------------------------------------------------------------------------------
         AppEvent getNextEvent();
+
+        //--------------------------------------------------------------------------------------------------------------
         EventQueue& getEventQueue() const;
+
+        NativeEventListeners& getNativeEventListeners();
+        const NativeEventListeners& getNativeEventListeners() const;
+
+    private:
+        virtual Vector<uptr<NativeEvent>> _fetchNativeEvents() = 0; // TODO: more efficient way to pass native events?
 
     private:
         mutable EventQueue _eventQueue; // change of the queue does not imply change of Mgr
+        NativeEventListeners _nativeEventListeners;
     };
 }
