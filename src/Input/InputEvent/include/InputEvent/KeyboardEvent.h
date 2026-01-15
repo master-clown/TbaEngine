779f65fad3c9601e3b69ff0e66e8e74b39aeb8ff
Abdulla Abakarov <abakarov97@gmail.com>
2026-01-15 11:44:03 +0400

    Add `str(KeyboardEvent)`

--- src/Input/InputEvent/include/InputEvent/KeyboardEvent.h
+++ src/Input/InputEvent/include/InputEvent/KeyboardEvent.h
@@ -3,9 +3,13 @@
 #include <InputEvent/KeyboardEventDetails.h>
 #include <InputEvent/KeyboardEventKind.h>
 
+#include <Common/String.h>
 #include <EventInfo/EventInfo.h>
 
 //======================================================================================================================
 namespace input_event {
     using KeyboardEvent = event_info::EventInfo<KeyboardEventKind, KeyboardEventDetails>;
+
+    //------------------------------------------------------------------------------------------------------------------
+    String str(const KeyboardEvent&);
 }
