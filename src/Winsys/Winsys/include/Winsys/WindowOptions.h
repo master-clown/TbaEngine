67fa308657c013a540d0d0a4c5626071048c85f3
Abdulla Abakarov <abakarov97@gmail.com>
2026-01-27 13:25:31 +0400

    Allow to specify `RendererType` to be prepared for during `Window` creation

--- src/Winsys/Winsys/include/Winsys/WindowOptions.h
+++ src/Winsys/Winsys/include/Winsys/WindowOptions.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <Common/Stl/Optional.h>
 #include <Common/String.h>
+#include <RendererType/RendererType.h>
 
 #include <cstdint>
 
@@ -10,5 +12,6 @@ namespace winsys {
         String wndTitle;
         uint16_t wndWidth = 0;
         uint16_t wndHeight = 0;
+        Optional<renderer_type::RendererType> initForRendererOfType = {};
     };
 }
