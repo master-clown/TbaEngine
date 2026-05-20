988e3252e069e105f4213f0ed58443bb2cfe8cf2
Abdulla Abakarov <abakarov97@gmail.com>
2026-05-20 10:35:17 +0200

    Minor reformulation of `PrimitiveId` class invariants

--- src/Render/Render2d/src/PrimitiveId.cpp
+++ src/Render/Render2d/src/PrimitiveId.cpp
@@ -9,12 +9,10 @@ std::atomic<PrimitiveId::RawId> PrimitiveId::_nextId = 0;
 //======================================================================================================================
 void PrimitiveId::_staticAssertsForInvariants()
 {
-    static constexpr uint64 bitsInByte = 8;
-
     static_assert(std::is_unsigned_v<RawId>);
 
-    static_assert(_idBitOffset < bitsInByte * sizeof(RawId));
-    static_assert(_primitiveTypeBitOffset < bitsInByte * sizeof(RawId));
+    static_assert(_bitOffsetForId < CHAR_BIT * sizeof(RawId));
+    static_assert(_bitOffsetForPrimitiveType + _numOfBitsForPrimitiveType <= CHAR_BIT * sizeof(RawId));
 
     static_assert((_primitiveTypeMask & _idMask) == 0);
     static_assert((_primitiveTypeMask | _idMask) == std::numeric_limits<RawId>::max());
@@ -48,7 +46,7 @@ namespace geometry_2d {
 //----------------------------------------------------------------------------------------------------------------------
 auto PrimitiveId::_generateNewId(const RawId primitiveType) -> PrimitiveId
 {
-    const auto typePart = primitiveType << _primitiveTypeBitOffset;
+    const auto typePart = primitiveType << _bitOffsetForPrimitiveType;
     const auto idPart = PrimitiveId::_nextId.fetch_add(1) & _idMask;
 
     return PrimitiveId{typePart | idPart};
@@ -59,7 +57,7 @@ template <>
 PrimitiveId PrimitiveId::generate<geometry_2d::Point2d>()
 {
     static constexpr RawId typeIdx = 0;
-    static_assert(typeIdx < _maxAvailablePrimitiveTypes);
+    static_assert(typeIdx < _numOfAvailablePrimitiveTypes);
 
     return PrimitiveId::_generateNewId(typeIdx);
 }
@@ -69,7 +67,7 @@ template <>
 PrimitiveId PrimitiveId::generate<geometry_2d::Line>()
 {
     static constexpr RawId typeIdx = 1;
-    static_assert(typeIdx < _maxAvailablePrimitiveTypes);
+    static_assert(typeIdx < _numOfAvailablePrimitiveTypes);
 
     return PrimitiveId::_generateNewId(typeIdx);
 }
@@ -79,7 +77,7 @@ template <>
 PrimitiveId PrimitiveId::generate<geometry_2d::Triangle>()
 {
     static constexpr RawId typeIdx = 2;
-    static_assert(typeIdx < _maxAvailablePrimitiveTypes);
+    static_assert(typeIdx < _numOfAvailablePrimitiveTypes);
 
     return PrimitiveId::_generateNewId(typeIdx);
 }
