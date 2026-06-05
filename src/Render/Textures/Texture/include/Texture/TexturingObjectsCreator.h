4e58f2825e9340e4387ddb2fefb2c6c63d333dfa
Abdulla Abakarov <abakarov97@gmail.com>
2026-06-05 16:51:00 +0400

    Rename `TexturingObjectsCreator::create()` to `createTexture()`

--- src/Render/Textures/Texture/include/Texture/TexturingObjectsCreator.h
+++ src/Render/Textures/Texture/include/Texture/TexturingObjectsCreator.h
@@ -16,7 +16,7 @@ namespace texture {
     public:
         virtual ~TexturingObjectsCreator();
 
-        virtual uptr<Texture> create(const TextureData&) = 0;
+        virtual uptr<Texture> createTexture(const TextureData&) = 0;
         virtual uptr<TextureSampler> createSampler(const TextureSamplingParameters&) = 0;
     };
 }
