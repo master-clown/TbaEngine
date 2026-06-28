35b357e3d12369ecf2e0f7b9faf10b9b3a263874
Abdulla Abakarov <abakarov97@gmail.com>
2026-06-28 17:07:04 +0400

    Support texture info in vertex data and in shaders

--- src/Render/Impl/OpenGl/OpenGlRender2d/src/OglVertexBuffer.cpp
+++ src/Render/Impl/OpenGl/OpenGlRender2d/src/OglVertexBuffer.cpp
@@ -26,15 +26,35 @@ OglVertexBuffer::OglVertexBuffer(const VaoId vaoId)
 
     _rebindVboToVao();
 
+    using AttribIdx = GLuint;
+    using Normalization = decltype(GL_TRUE);
+
     static constexpr GLuint vboBindingPoint = 0;
 
-    glEnableVertexArrayAttrib(_vaoId, 0);
-    glVertexArrayAttribFormat(_vaoId, 0, 2, GL_FLOAT, GL_TRUE, offsetof(OglVertexInfo, x));
-    glVertexArrayAttribBinding(_vaoId, 0, vboBindingPoint);
+    glEnableVertexArrayAttrib(_vaoId, AttribIdx{0});
+    glVertexArrayAttribFormat(_vaoId, AttribIdx{0}, 2, GL_FLOAT, Normalization{GL_TRUE}, offsetof(OglVertexInfo, x));
+    glVertexArrayAttribBinding(_vaoId, AttribIdx{0}, vboBindingPoint);
 
-    glEnableVertexArrayAttrib(_vaoId, 1);
-    glVertexArrayAttribFormat(_vaoId, 1, 4, GL_UNSIGNED_BYTE, GL_TRUE, offsetof(OglVertexInfo, r));
-    glVertexArrayAttribBinding(_vaoId, 1, vboBindingPoint);
+    glEnableVertexArrayAttrib(_vaoId, AttribIdx{1});
+    glVertexArrayAttribFormat(_vaoId,
+                              AttribIdx{1},
+                              4,
+                              GL_UNSIGNED_BYTE,
+                              Normalization{GL_TRUE},
+                              offsetof(OglVertexInfo, r));
+    glVertexArrayAttribBinding(_vaoId, AttribIdx{1}, vboBindingPoint);
+
+    glEnableVertexArrayAttrib(_vaoId, AttribIdx{2});
+    glVertexArrayAttribIFormat(_vaoId,
+                               AttribIdx{2},
+                               2,
+                               GL_UNSIGNED_INT,
+                               offsetof(OglVertexInfo, handleLower4Bytes));
+    glVertexArrayAttribBinding(_vaoId, AttribIdx{2}, vboBindingPoint);
+
+    glEnableVertexArrayAttrib(_vaoId, AttribIdx{3});
+    glVertexArrayAttribFormat(_vaoId, AttribIdx{3}, 2, GL_FLOAT, Normalization{GL_FALSE}, offsetof(OglVertexInfo, u));
+    glVertexArrayAttribBinding(_vaoId, AttribIdx{3}, vboBindingPoint);
 }
 
 //======================================================================================================================
@@ -47,6 +67,61 @@ auto OglVertexBuffer::getVboId() const -> VboRawId
 TypedOglBuffer<OglVertexInfo>& OglVertexBuffer::getUnderlyingBuffer()
 {
     return _buffer;
+}
+
+//======================================================================================================================
+const std::string_view& OglVertexBuffer::getVertexShaderText()
+{
+    static constexpr std::string_view vertexShader = R"(
+#version 460 core
+
+layout(location = 0) in vec2 inVertexPosition; // NDC coordinate system
+layout(location = 1) in vec4 inVertexColor;
+layout(location = 2) in uvec2 inTextureHandle;
+layout(location = 3) in vec2 inTextureCoords;
+
+layout(location = 0) out vec4 outVertexColor;
+layout(location = 1) out flat uvec2 outTextureHandle;
+layout(location = 2) out vec2 outTextureCoords;
+
+void main() 
+{
+    gl_Position = vec4(inVertexPosition, 0.0, 1.0);
+    outVertexColor = inVertexColor;
+    outTextureHandle = inTextureHandle;
+    outTextureCoords = inTextureCoords;
+}
+)";
+
+    return vertexShader;
+}
+
+//======================================================================================================================
+const std::string_view& OglVertexBuffer::getFragmentShaderText()
+{
+    static constexpr std::string_view fragmentShader = R"(
+#version 460 core
+#extension GL_ARB_bindless_texture : require
+
+layout(location = 0) in vec4 inVertexColor;
+layout(location = 1) in flat uvec2 inTextureHandle;
+layout(location = 2) in vec2 inTextureCoords;
+
+layout(location = 0) out vec4 fragmentColor;
+
+void main()
+{
+    if(inTextureHandle == uvec2(0, 0)) {
+        fragmentColor = inVertexColor;
+        return;
+    }
+
+    sampler2D texSampler = sampler2D(inTextureHandle);
+    fragmentColor = texture(texSampler, inTextureCoords);
+}
+)";
+
+    return fragmentShader;
 }
 
 //======================================================================================================================
--- src/Render/Impl/OpenGl/OpenGlRender2d/src/OglVertexInfo.h
+++ src/Render/Impl/OpenGl/OpenGlRender2d/src/OglVertexInfo.h
@@ -8,11 +8,20 @@ namespace opengl_render_2d {
     struct OglVertexInfo final {
         using NdcCoord = geometry_2d::ScreenCoordinate;
 
+        // TODO: change this ad-hoc when the `libContent` is properly designed
+        using TextureCoord = float;
+
         NdcCoord x = 0.0f;
         NdcCoord y = 0.0f;
         uint8 r = 0;
         uint8 g = 0;
         uint8 b = 0;
         uint8 a = 0;
+
+        // TODO: change this ad-hoc when the `libContent` is properly designed
+        uint32 handleLower4Bytes = 0;
+        uint32 handleUpper4Bytes = 0;
+        float u = 0.0;
+        float v = 0.0;
     };
 }
